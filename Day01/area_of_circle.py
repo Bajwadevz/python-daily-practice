f2af23bb5b6947feb0d75f8a4fa88782b1a5fa87
Bajwadevz <shahzebahmed1000@gmail.com>
2025-05-05 16:48:05 +0500

    Day 1 - Completed all 5 beginner scripts

---
+++ Day01/area_of_circle.py
@@ -0,0 +1,7 @@
+radius = (input("EnterRadius: "))
+area = 3.14 * radius ** 2
+print("The Area is ", radius)
+
+radius = float(input("Enter radius: "))  # ✅ typecast to float
+area = 3.14 * radius ** 2
+print("Area:", area)
---
+++ Day01/square_cube.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number: "))
+
+square = num ** 2
+cube = num ** 3
+
+print("square is: ", int(square))
+print("square is: ", int(cube))
